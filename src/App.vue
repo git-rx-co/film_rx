e72807c0f998964a5b0f4beca6d7e51992b34635
rcode4243 <74647496+rcode4243@users.noreply.github.com>
2023-09-12 11:43:20 +0600

    init

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,37 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
+
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import NavBar from './components/Global/NabBar.vue' 
+import BannerMain from './components/Global/Banner.vue' 
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    NavBar,BannerMain
   }
 }
 </script>
 
 <style>
+ *{
+  font-family: 'Poppins', sans-serif;
+	font-weight: 500;
+	font-style: normal;
+	font-size: 14px;
+  color: #ffffff;
+  background: #1B252E;
+ }
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+ 
 }
 </style>
+
+<template>
+  <NavBar />
+  <BannerMain />
+  <h1>Example heading <span class="badge bg-secondary">New</span></h1>
+  
+
+</template>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,35 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue"
+import App from "./App.vue"
+import "bootstrap/dist/css/bootstrap.css"
+import "bootstrap-vue/dist/bootstrap-vue.css"
+
+
+import "./assets/css/bootstrap.min.css"; 
+import "./assets/css/animate.min.css"; 
+import "./assets/css/aos.css"; 
+import "./assets/css/default.css"; 
+import "./assets/css/flaticon.css"; 
+import "./assets/css/fontawesome-all.min.css"; 
+import "./assets/css/style.css"; 
+import "./assets/css/magnific-popup.css"; 
+import "./assets/css/odometer.css"; 
+import "./assets/css/responsive.css"; 
+import "./assets/css/slick.css"; 
+import "./assets/css/owl.carousel.min.css"; 
+ 
+// import "./assets/js/vendor/jquery-3.6.0.min.js"
+// import "./assets/js/popper.min.js"
+// import "./assets/js/bootstrap.min.js"
+// import "./assets/js/isotope.pkgd.min.js"
+// import "./assets/js/imagesloaded.pkgd.min.js"
+// import "./assets/js/jquery.magnific-popup.min.js"
+// import "./assets/js/owl.carousel.min.js"
+// import "./assets/js/jquery.odometer.min.js"
+// import "./assets/js/jquery.appear.js"
+// import "./assets/js/slick.min.js"
+// // import "./assets/js/ajax-form.js"
+// // import "./assets/js/wow.min.js"
+// import "./assets/js/aos.js"
+// import "./assets/js/plugins.js" 
 
 createApp(App).mount('#app')
